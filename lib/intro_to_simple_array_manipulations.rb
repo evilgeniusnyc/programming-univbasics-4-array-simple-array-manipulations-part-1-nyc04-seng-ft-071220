f1ddf0abba02163c31554ffc7bb76ececcbee228
Steven Williams <evilgeniusnyc@me.com>
2020-07-18 15:24:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,14 +7,12 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end
-
 #This method takes in parameter of an Array and uses the .pop method to remove the last element from the array and return that element.
 def using_pop(array)
   array.pop
 end
-
 #This method takes in a parameter of an Array and uses the .shift method to remove the first item and return it.
-def using_shift
+def using_shift(array)
   array.shift
 end
 
