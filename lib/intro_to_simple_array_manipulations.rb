31f5f487809ff6486f24f7b93fb64f66096b1b77
Steven Williams <evilgeniusnyc@me.com>
2020-07-18 15:28:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,16 +7,16 @@ end
 def using_unshift(array, string)
   array.unshift(string)
 end
-#This method takes in parameter of an Array and uses the .pop method to remove the last element from the array and return that element.
+#This method takes in parameter of an Array and uses the .pop method to remove the last element from the array and return that element. ** PASS
 def using_pop(array)
   array.pop
 end
-#This method takes in a parameter of an Array and uses the .shift method to remove the first item and return it.
+#This method takes in a parameter of an Array and uses the .shift method to remove the first item and return it.  ** PASS
 def using_shift(array)
   array.shift
 end
-
 #This method takes in a parameter of an Array and uses the .shift method. This time, pass an argument, 2, into .shift to remove and return the first two items from the array.
-def shift_with_args
-  
+def shift_with_args(array)
+  array.shift
+  array.shift
 end
