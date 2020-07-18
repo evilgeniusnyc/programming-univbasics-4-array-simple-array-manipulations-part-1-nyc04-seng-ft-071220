49f74c142284026400fe573a89741f1cc640e2fa
Steven Williams <evilgeniusnyc@me.com>
2020-07-18 15:12:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+# This method takes in two parameters, an Array and a String. It adds that string to the end of the array using the .push method. **PASS
+def using_push(array, string)
+  array.push(string)
+end
+
+# This method takes in two parameters, an Array and a String and adds that string to the front of the array using the .unshift method. ** PASS
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+#This method takes in parameter of an Array and uses the .pop method to remove the last element from the array and return that element.
+def using_pop
+  array.pop
+end
+
+#This method takes in a parameter of an Array and uses the .shift method to remove the first item and return it.
+def using_shift
+  array.shift
+end
+
+#This method takes in a parameter of an Array and uses the .shift method. This time, pass an argument, 2, into .shift to remove and return the first two items from the array.
+def shift_with_args
+  
+end
